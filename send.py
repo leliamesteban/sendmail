4374962856cde3b1cf2ca2836761fb043eb03b71
liam user <liam@debian>
2019-05-22 19:20:07 +0000

    Use TLS/SSL
    smtplib provides the function SMTP_SSL that uses TLS/SSL to secure the
    connection.
    
    Alternatively, you can also create an insecure connection and
    "upgrade" it using .starttls()
    
    Note that the port changes and that the .helo function is optional
    when using TLS

--- send.py
+++ send.py
@@ -1,6 +1,4 @@
 import smtplib
 
-# create insecure connection to ('server', port)
-server = smtplib.SMTP('smtp.gmail.com', 587)
-# identify to the server
-server.helo()
+# create secure (TLS/SSL) connection to ('server', port)
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
