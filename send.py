24417d7026ff02d1ae140a532ee79e6dfc6d1c39
liam user <liam@debian>
2019-05-22 19:22:56 +0000

    Format email
    smtplib uses plain text formatting to send emails, so we just use
    the standard format of From: email, To: email etc
    
    The variables are concatenated into a string using f strings,
    available from python 3.6 onwards

--- send.py
+++ send.py
@@ -1,4 +1,20 @@
 import smtplib
 
+sender = 's3nd3m41L@gmail.com'
+to = ['s3nd3m41L@gmail.com']
+gmail_user = 's3nd3m41L@gmail.com'
+gmail_password = 'lapel status preamble petticoat cement'
+subject = ''
+body = ''
+
+email = (
+        f'From: {sender}'
+        f'To: {to}'
+        f'Subject: {subject}'
+
+        f'{body}'
+        )
+
 # create secure (TLS/SSL) connection to ('server', port)
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(gmail_user, gmail_password)
