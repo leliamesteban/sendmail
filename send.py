65638a9797c327c671f3b612a58dd4d4e2628731
liam user <liam@debian>
2019-05-22 21:56:49 +0000

    Use try block to catch exceptions

--- send.py
+++ send.py
@@ -14,8 +14,12 @@ Subject: {}
 {}
 """.format(sender, to, subject, body)
 
-# create secure (TLS/SSL) connection to ('server', port)
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login(gmail_user, gmail_password)
-server.sendmail(sender, to, email)
-server.close()
+try:
+    # create secure (TLS/SSL) connection to ('server', port)
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sender, to, email)
+except Exception as exception:
+    print(exception)
+finally:
+    server.close()
